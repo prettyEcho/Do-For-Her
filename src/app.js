de760c30bfb60238d57ca3cc0a7a0958b00214c3
jiancheng.zhang <jiancheng.zhang@qunar.com>
2019-01-14 19:45:05 +0800

    fix: 修复启动

--- src/app.js
+++ src/app.js
@@ -28,29 +28,7 @@ class App extends Component {
     // get mobx store
     this.MOBX_STORE = getInitstore()
 
-    Taro.login({
-      success: function (res) {
-        if (res.code) {
-          console.log('res.code', res.code)
-          /* Taro.request({
-            url: 'https://test.com/login',
-            data: {
-              username: 'zhangsan', // 用户输入的账号
-              password: 'pwd123456', // 用户输入的密码
-              code: res.code
-            },
-            success: function (res) {
-              // 登录成功
-              if (res.statusCode === 200) {
-                console.log(res.data.sessionId)// 服务器回包内容
-              }
-            }
-          }) */
-        } else {
-          console.log('获取用户登录态失败！' + res.errMsg)
-        }
-      }
-    });
+
   }
 
 
--- src/model/home.js
+++ src/model/home.js
@@ -1,11 +1,34 @@
 import { action, observable } from 'mobx'
+import Taro from '@tarojs/taro'
+
 
 export default class Home {
-  @observable show
 
   @action
-  changeShow () {
-    this.show = true
+  login() {
+    Taro.login({
+      success: function (res) {
+        if (res.code) {
+          console.log('res.code', res.code)
+          /* Taro.request({
+            url: 'https://test.com/login',
+            data: {
+              username: 'zhangsan', // 用户输入的账号
+              password: 'pwd123456', // 用户输入的密码
+              code: res.code
+            },
+            success: function (res) {
+              // 登录成功
+              if (res.statusCode === 200) {
+                console.log(res.data.sessionId)// 服务器回包内容
+              }
+            }
+          }) */
+        } else {
+          console.log('获取用户登录态失败！' + res.errMsg)
+        }
+      }
+    });
   }
 
 } 
