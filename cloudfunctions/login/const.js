1feaa167ab18c6d55da765a61cf3892ad636f421
jiancheng.zhang <jiancheng.zhang@qunar.com>
2019-01-23 20:38:34 +0800

    feat: 添加登录数据库

---
+++ cloudfunctions/login/const.js
@@ -0,0 +1,9 @@
+exports.MINI_INFO = {
+    appid: 'wx4e40d5908945058a',
+    secret: '02bf3bc3bb919ceb2661aff7bb15170c'
+}
+
+exports.RES_MSG = {
+    NO_CODE: '请重新登录', // 未获取到CODE
+    NO_SESSION: '请重新登录' // 未获取到SESSION
+}
--- cloudfunctions/login/index.js
+++ cloudfunctions/login/index.js
@@ -1,16 +1,52 @@
 // 云函数入口文件
 const cloud = require('wx-server-sdk')
+const { MINI_INFO, RES_MSG } = require('./const.js')
 
 cloud.init()
+
+const db = wx.cloud.database()
 
 // 云函数入口函数
 exports.main = async (event, context) => {
   const wxContext = cloud.getWXContext()
-  console.log('+++++', event.code)
-  return {
-    event,
-    openid: wxContext.OPENID,
-    appid: wxContext.APPID,
-    unionid: wxContext.UNIONID
+
+  if (!event.code) {
+    return {
+      errCode: '500',
+      errMsg: RES_MSG.NO_CODE
+    }
   }
+
+  // 换取用户session
+  const { openid, session_key } = await getSession(event.code)
+
+  if (!openid || !session_key) {
+    return {
+      errCode: '500',
+      errMsg: RES_MSG.NO_SESSION
+    }
+  }
+
+  db.collection('user').where({
+    openid: openid
+  }).get().then(res => {
+    console.log('res', res)
+    return {
+      errorCode: '200',
+      data: {
+        token: res.token
+      }
+    }
+  })
+}
+
+function getSession(code) {
+  return Taro.request({
+    url: 'https://api.weixin.qq.com/sns/jscode2session',
+    data: {
+      appid: MINI_INFO.appid,
+      secret: MINI_INFO.secret,
+      js_cdoe: code
+    }
+  })
 }
